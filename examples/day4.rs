ce5a31bf59efc6783792ea58897662ddb076afc3
benjamin.lewington <benjamin.lewington@efficioconsulting.com>
2022-12-04 20:10:45 +0000

    day 4 answers

--- examples/day4.rs
+++ examples/day4.rs
@@ -7,22 +7,22 @@ fn main() -> Result<()> {
     let contents = read_day_data(4)?;
     let section_range_pair_parser = SectionRangePair::parser(',', '-');
 
+    // 4.1
     let l = &contents
         .split("\r\n")
         .map(|s| section_range_pair_parser(s).unwrap())
         .filter(|srp| srp.is_subset())
         .count();
 
-    // 4.1
     println!("Day 4, Answer 1: {}", l);
 
+    // 4.2
     let l = &contents
         .split("\r\n")
         .map(|s| section_range_pair_parser(s).unwrap())
         .filter(|srp| !srp.is_no_overlap())
         .count();
 
-    // 4.1
     println!("Day 4, Answer 2: {}", l);
 
     Ok(())
